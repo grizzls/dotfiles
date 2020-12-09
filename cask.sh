6ebe0aca97bf01a5adb77d54945d8e3011050f35
Chris Sowley <csowley@csowley-Mac.local>
2020-12-08 23:32:02 -0500

    testing cask installs

---
+++ cask.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+work=(
+    google-chrome
+    visual-studio-code
+    1password
+    iterm2
+    google-cloud-sdk
+    docker
+    slack
+    postman
+    microsoft-teams
+    microsoft-office
+    macpass
+    zoom
+)
+
+brew install --cask ${work[@]}
